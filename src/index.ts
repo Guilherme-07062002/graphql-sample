41cc0074370c677a424340e29b0e6fc5a02543a9
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-25 19:53:37 -0300

    feat: config 'graphql' endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import http from 'http';
 
+import { graphqlHTTP } from 'express-graphql';
+import { schema } from './schema';
+
 const app = express();
 const server = http.createServer(app);
 
@@ -10,6 +13,14 @@ app.get('/', (req, res) => {
   });
   res.send(response);
 });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 server.listen(3000, () => {
   console.log('\nlistening on 3000');
